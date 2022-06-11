08d31fa0507ad91b9674a43c9aefb027ed0b102c
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-11 09:25:33 +0530

    Create deleteNode.cpp

---
+++ Binary Search Tree/deleteNode.cpp
@@ -0,0 +1,32 @@
+//Given a node value, we need to delete it from BST, if it is present in BST, and return our root...
+
+//Helper succcessor method, that return the next successor, (This is used only when right and left sub-trees are not null)
+Node *successor(Node *root) {
+    Node *curr = root->right;
+    while(curr!=NULL && curr->left!=NULL) curr = curr->left;
+    return curr;
+}
+
+// Primary Function
+Node *deleteNode(Node *root, int target) {
+    if(root==NULL) return root;
+    if(root->val > target) root->left = deleteNode(root->left);
+    else if(root->val < target) root->right = deleteNode(root->right);
+    else {
+        if(root->left == NULL) {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        } else if(root->right == NULL) {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        } else {
+            Node *temp = successor(root);
+            root->key = temp->key;
+            root->right = deleteNode(root->right, temp->key);
+        }
+    }
+    
+    return root;
+}
